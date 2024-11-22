6e08dd870cd0e05247eb29c93f541128c959c045
marcela-05 <132473980+marcela-05@users.noreply.github.com>
2024-11-22 17:01:33 -0300

    Update diretores.py

--- diretores.py
+++ diretores.py
@@ -3,13 +3,6 @@ from utils import gerar_id_unico
 # Dados mockados
 diretores = []
 
-def criar_diretor(nome_diretor):
-    if any(d['nome'] == nome_diretor for d in diretores):
-        return {"status": "erro", "mensagem": "Diretor já cadastrado."}
-    diretor = {"id": gerar_id_unico(diretores), "nome": nome_diretor}
-    diretores.append(diretor)
-    return {"status": "sucesso", "diretor": diretor}
-
 def listar_diretor(diretor_id):
     for d in diretores:
         if d['id'] == diretor_id:
